d8ff0ab07cfc70a62f4aded5ca7d39742375ba16
Sam <maina.sam@outlook.com>
2022-12-02 17:00:41 +0300

    python common utility functions

---
+++ util_functions.py
@@ -0,0 +1,15 @@
+# Code to demonstrate some of python's built-in functions
+my_cars = ["BMW","Toyota", "Jeep", "Mercedes", "Honda"]
+
+# 1: Enumerate function
+print(list(enumerate(my_cars)))
+
+# 2: Sort the list
+my_cars.sort()
+print(my_cars)
+
+# 3: Length of object (my_car)
+print(len(my_cars))
+
+# 4: Power of a number
+print(pow(25, 4))
